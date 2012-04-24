84d9d294d2efceff76a899aa6bcbe89fe5fd1a10
iain.steers <iain.s@ntlworld.com>
2012-04-24 18:15:41 +0200

    Test signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def new
       @user = User.new
   end
+  
   def create
       @user = User.new(params[:user])
       if @user.save
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,28 +4,40 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     current_user = user
   end
-  
+
   def signed_in?
-     !current_user.nil?
+    !current_user.nil?
   end
-  
+
   def current_user=(user)
-     @current_user = user
+    @current_user = user
   end
-  
+
   def current_user
-     @current_user ||= user_from_remember_token
+    @current_user ||= user_from_remember_token
   end
-  
-  def sign_out
-      current_user = nil
-      cookies.delete(:remember_token)
+
+  def current_user?(user)
+    user == current_user
   end
-  
-  private
-  
-    def user_from_remember_token
-	remember_token = cookies[:session][:remember_token]
-	User.find_by_remember_token(remember_token) unless remember_token nil?
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
     end
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  private
+
+    def user_from_remember_token
+      remember_token = cookies[:remember_token]
+      User.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
+    
 end
