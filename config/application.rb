3c71c4d8de3164b3a1ecf84a410c4338b5f862a5
iain.steers <iain.s@ntlworld.com>
2012-04-26 13:59:02 +0200

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module SampleApp
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
+    config.active_record.whitelist_attributes = true
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
